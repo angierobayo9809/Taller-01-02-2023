61302b072985a2fe9a62a91f6459c4ccdaf97f7d
angierobayo09 <arobayo@ibiscreativa.com>
2023-02-01 20:40:17 -0500

    Ejercicio 5

---
+++ pedir_Datos_Por_Prompt_01022023/main.js
@@ -0,0 +1,6 @@
+/* 5.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <head></ head>,
+ que has de grabar en una carpeta llamada pedir_Datos_Por_Prompt_FechaActual, En la ventana saldrá un texto que diga 
+ “Ingresar un numero” capturar el valor en un variable y mostrar el valor por medio de la consola o de la ventana emergente.*/
+
+ let numero = prompt("Ingrese su número favorito");
+ alert("Su número favorito es " + numero);
