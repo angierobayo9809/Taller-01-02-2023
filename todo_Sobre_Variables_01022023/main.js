923d235135eb24c65774f920afa6439c96b47256
angierobayo09 <arobayo@ibiscreativa.com>
2023-02-01 20:35:56 -0500

    Ejercicio 4

---
+++ todo_Sobre_Variables_01022023/main.js
@@ -0,0 +1,12 @@
+/*4.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <head></ head>, 
+que has de grabar en una carpeta llamada todo_Sobre_Variables_FechaActual, y declarar variable y asignar a cada una 
+de ellas un valor de tipo String,Number,Booleano y mostrar los valores por medio de la consola o de la ventana emergente,
+ Nota la variable de tipo constante const el tipo de valor es opcional.*/
+ 
+ let nombre = new String("Angie Robayo Jaimes");
+ let estatura = new Number("1.63");
+ let practicaDeporte = new Boolean(true);
+ const deporte = new String("Ultimate Frisbee");
+
+
+ console.info("Mi nombre es "+ nombre +", yo mido "+ estatura +" y es "+ practicaDeporte + " que yo practico "+deporte );
