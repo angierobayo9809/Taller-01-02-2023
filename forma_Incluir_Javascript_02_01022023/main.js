e520d0fe833d86bb6b527cdc3ba93f869b992640
angierobayo09 <arobayo@ibiscreativa.com>
2023-02-01 20:22:04 -0500

    Ejercicio 3

--- forma_Incluir_Javascript_02_01022023/main.js
+++
@@ -1,7 +0,0 @@
-/* 2.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <body></body>,
- que has de grabar en una carpeta llamada forma_Incluir_Javascript_02_FechaActual, mostrar un mensaje por medio de la 
- consola o de la ventana emergente un mensaje “Conexión con el archivo JavaScript Exitoso” .  */
-
-confirm("Conexión con el archivo JavaScript Exitoso");
-
-
